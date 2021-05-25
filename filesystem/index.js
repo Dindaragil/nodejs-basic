c5a92b2d2a54d438c8355a4f3a53c5da2f9426a3
Dindaragil <dindawardani36@gmail.com>
2021-05-25 18:04:31 +0700

    filesystem

---
+++ filesystem/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { resolve } = require('path');
+
+const fileReadCallBack = (error, data) => {
+    if(error) {
+        console.log('Gagal membaca berkas!')
+    }
+    console.log(data);
+}
+
+fs.readFile(resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallBack);
